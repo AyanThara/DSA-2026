ec6760db12a5571b95244614bfca9ab98ee01ea2
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-02-05 20:06:16 +0530

    Create First and Last Position of an Element In Sorted Array using binary search.cpp

---
+++ Coding ninjas/First and Last Position of an Element In Sorted Array using binary search.cpp
@@ -0,0 +1,33 @@
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k){
+  int first = -1, last = -1;
+    int start = 0, end = n - 1;
+    while(start <= end){
+        int mid = start + (end - start) / 2;
+        if(arr[mid] == k){
+            first = mid;
+            end = mid - 1;   
+        }
+        else if(arr[mid] < k){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    start = 0;
+    end = n - 1;
+    while(start <= end){
+        int mid = start + (end - start) / 2;
+        if(arr[mid] == k){
+            last = mid;
+            start = mid + 1; 
+        }
+        else if(arr[mid] < k){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    return {first, last};
+}
