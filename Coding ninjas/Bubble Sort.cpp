c2874b545bb57368b12db1a44bdeeec106074f7b
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-02-12 18:41:13 +0530

    Create Bubble Sort.cpp

---
+++ Coding ninjas/Bubble Sort.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+Approach:
+- Compare adjacent elements
+- Swap if needed
+- Stop early if no swaps happen
+
+*/
+
+void bubbleSort(vector<int>& arr) {
+    int n = arr.size();
+    for(int i = 0; i < n - 1; i++) {
+        bool swapped =false;//take a variable nd make it false 
+        for(int j = 0; j < n - i - 1; j++) {
+            if(arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;//if swap then update to true 
+            }
+        }
+
+        if(swapped==false) {//check if no swap then break so we can optimize the code
+          break; 
+        }
+    }
+}
