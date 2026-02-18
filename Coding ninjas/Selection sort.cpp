265a57fa61cec7dff37e48bbcc403b2eba5376c7
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-02-18 11:16:53 +0530

    Update Selection sort.cpp

--- Coding ninjas/Selection sort.cpp
+++ Coding ninjas/Selection sort.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h> 
-void selectionSort(vector<int>& arr, int n)
-{   
+void selectionSort(vector<int>& arr, int n){
     for(int i=0;i<n;i++){
         int min=i;
         for(int j=i;j<n;j++){
