94b411943ae7d679f53f6a6b9e268861fefda4bb
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-03-06 01:21:19 +0530

    Update Reverse_String.cpp

--- String/Reverse_String.cpp
+++ String/Reverse_String.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <string>
 using namespace std;
 int main() {
@@ -6,4 +6,21 @@ int main() {
     for(int i = str.length() - 1; i >= 0; i--) {
         cout << str[i];
     }
+}
+*/
+//Two pointer approaoch
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str = "ayan";
+    int start = 0;
+    int end = str.length() - 1;
+
+    while(start < end) {
+        swap(str[start], str[end]);
+        start++;
+        end--;
+    }
+    cout << str;
 }
