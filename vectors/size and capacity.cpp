275884787ea8d059d9bbc0ff0101c24e4b00fa3b
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-03-29 13:53:58 +0530

    Update size and capacity.cpp

--- vectors/size and capacity.cpp
+++ vectors/size and capacity.cpp
@@ -6,6 +6,7 @@ int main(){
     vec.push_back(0);
     vec.push_back(1);
     vec.push_back(2);
-    cout << "Size: " << vec.size() << endl;
-    cout << "Capacity: " << vec.capacity() << endl;
+    cout << "Size: " << vec.size() << endl;//3
+    cout << "Capacity: " << vec.capacity() << endl;//4
 }
+//in next iteratoion if 2 more elements are addedd then size will be 5 but capacity will be double of 4 i.e 8
