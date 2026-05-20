d266cb181e1c0f5401df5a8975c2f87b810f1646
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-05-20 21:16:59 +0530

    Update Pair_Sum_Sorted_Array.cpp

--- Arrays/Pair_Sum_Sorted_Array.cpp
+++ Arrays/Pair_Sum_Sorted_Array.cpp
@@ -39,3 +39,47 @@ public:
         return {};
     }
 };
+
+
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> pairSum(vector<int> arr, int target){
+
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while(left < right){
+
+        int sum = arr[left] + arr[right];
+
+        if(sum == target){
+            return {left, right};
+        }
+
+        else if(sum < target){
+            left++;
+        }
+
+        else{
+            right--;
+        }
+    }
+
+    return {};
+}
+
+int main(){
+
+    vector<int> arr = {1,2,3,4,5};
+    int target = 5;
+
+    vector<int> result = pairSum(arr, target);
+
+    cout << result[0] << " " << result[1];
+
+    return 0;
+}
