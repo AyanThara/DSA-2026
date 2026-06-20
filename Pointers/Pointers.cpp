6d784e4f3680e6ef1818d8351222cef6ebd84068
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-06-20 19:46:54 +0530

    Update Pointers.cpp

--- Pointers/Pointers.cpp
+++ Pointers/Pointers.cpp
@@ -3,6 +3,8 @@ using namespace std;
 int main() {
     int a = 10;
     int *ptr = &a;
-    cout << ptr << endl;   // prints address of a
-    cout << *ptr << endl;  // prints value stored at that address
+    cout << ptr << endl;   // prints address of a 
+    cout<<&a<<endl;//address of a
+    cout << *ptr << endl;  // prints value stored at that address //10
+    cout<<&ptr<<endl;//prints the address of pointer
 }
