4e04f0033bd9b1f7fbc24e9834c3bd31ec980700
AyanThara <151911407+AyanThara@users.noreply.github.com>
2026-06-20 19:40:53 +0530

    Update Pointers.cpp

--- Pointers/Pointers.cpp
+++ Pointers/Pointers.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
-int main(){
-  int a=10;
-  int *ptr=&a;
-  cout<<ptr<<endl;
-  cout<<*ptr<endl;
+int main() {
+    int a = 10;
+    int *ptr = &a;
+    cout << ptr << endl;   // prints address of a
+    cout << *ptr << endl;  // prints value stored at that address
 }
